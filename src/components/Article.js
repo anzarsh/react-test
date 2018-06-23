ebc62003c9929d3c59fec6356473fb049e30684c
anzarsh <anzarsh@mail.ru>
2018-06-23 18:40:56 +0300

    test react app

---
+++ src/components/Article.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+class Article extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    render() {
+        const {article} = this.props;
+        const body = this.state.isOpen && <section>{article.text}</section>;
+        return (
+            <div>
+                <h2>
+                    {article.title}
+                    <button onClick={() => this.handleClick()}>
+                        {this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
+                {body}
+                <h3>create date: {new Date(article.date).toDateString()}</h3>
+            </div>
+        );
+    }
+
+    handleClick() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+}
+
+export default Article;
